65bb6fcc68b16206ad179ee5ee69ca9f70147121
EtherealMochi <51897953+EtherealMochi@users.noreply.github.com>
2021-12-24 17:40:09 -0600

    add selection menu
    menu is added to display_menu function and quiz_dir variable updated to new quiz file location

--- quiz.sh
+++ quiz.sh
@@ -6,7 +6,7 @@ NC='\033[0m'
 
 # variables
 PS3="> "
-quiz_dir=(quiz_files/*)
+quiz_dir=(quiz_keys/*)
 question_key=()
 answer_key=()
 q_index=-1
@@ -18,4 +18,21 @@ function display_menu {
     echo "For answers that have multiple parts, separate them with a comma."
     echo  -e "Example: What are the 3 types of loops in bash scripting? ${GREEN}for, while, until${NC}"
     echo -e "\n"
+
+    echo "Choose your quiz below (q to quit):"
+    # select quiz and if anything other than the numbers of the files is chosen, exit script
+    select opt in "${quiz_dir[@]}"
+    do
+        case "$opt" in
+            "") echo "Exiting quiz..."
+                exit 1 ;;
+            *) quiz_file="$opt" 
+                break;;
+        esac
+    done
+    # loop through all lines of quiz file and split keys into arrays
+    while IFS="|" read -ra line; do
+	question_key+=("${line[0]}")
+	answer_key+=("${line[1]}")
+    done < "$quiz_file"
 }
