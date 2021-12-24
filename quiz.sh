6130cdc6bfa0cc9447e9e31783786a49b779ac7b
EtherealMochi <51897953+EtherealMochi@users.noreply.github.com>
2021-12-24 17:34:11 -0600

    add function for startup display
    function display_menu created and outputs quizz shell header to console

--- quiz.sh
+++ quiz.sh
@@ -10,3 +10,12 @@ quiz_dir=(quiz_files/*)
 question_key=()
 answer_key=()
 q_index=-1
+
+function display_menu {
+    echo ""
+    echo "QUIZZ SHELL"
+    echo "_________________________"
+    echo "For answers that have multiple parts, separate them with a comma."
+    echo  -e "Example: What are the 3 types of loops in bash scripting? ${GREEN}for, while, until${NC}"
+    echo -e "\n"
+}
